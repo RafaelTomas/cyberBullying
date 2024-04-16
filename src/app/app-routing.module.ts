9213d1cffd42452a39f037a00869ae04ef3a16a8
Rafael Tomas <rafael.tomas@mv.com.br>
2024-04-16 02:01:20 -0300

    add informtion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, //default route
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent }
 ];
 
